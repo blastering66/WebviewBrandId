a8a50ef5dac466f24a84c11495741e221361e4ca
ridhoCheers <ridho@yourcheersapp.com>
2018-12-12 13:29:55 +0700

    General: Firebase integrated

--- app/src/main/java/com/infobrand/MainApplication.java
+++ app/src/main/java/com/infobrand/MainApplication.java
@@ -1,7 +1,13 @@
 package com.infobrand;
 
 import android.app.Application;
+import android.support.annotation.NonNull;
+import android.util.Log;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 import com.google.firebase.messaging.FirebaseMessaging;
 
 public class MainApplication extends Application {
@@ -9,6 +15,25 @@ public class MainApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        FirebaseMessaging.getInstance().subscribeToTopic("brandid_notif");
+        FirebaseMessaging
+                .getInstance()
+                .subscribeToTopic("brandid_notif")
+        .addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                Log.d("SUBSCRIBE", "COMPLETE");
+            }
+        });
+
+        FirebaseInstanceId.getInstance().getInstanceId().addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+            @Override
+            public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                if (!task.isSuccessful()) {
+                    Log.e("SUBSCRIBE", "INSUCCUESS");
+                }
+                String token = task.getResult().getToken();
+                Log.e("INSTANCE ID", token);
+            }
+        });
     }
 }
